73970bf3216d7e6881f28a81954a49515275a870
Fahril Refiandi <fahril.refandi@gmail.com>
2026-08-22 11:47:22 +0700

    feat: add dynamic positioning logic to dropdown component and update logout item focus states

--- resources/views/vibe/dropdown/index.blade.php
+++ resources/views/vibe/dropdown/index.blade.php
@@ -28,8 +28,46 @@
 
 <div class="relative inline-block text-left" x-data="{ 
     open: false,
+    align: @js($align),
+    init() {
+        this.$watch('open', value => {
+            if (value) {
+                this.$nextTick(() => {
+                    this.adjustPosition();
+                });
+            }
+        });
+    },
     toggle() { this.open = !this.open },
     close() { this.open = false },
+    adjustPosition() {
+        if (!this.$refs.menuContainer) return;
+        let menu = this.$refs.menuContainer.parentElement; // The absolute wrapper
+        
+        // Remove dynamic classes
+        menu.classList.remove('left-0', 'right-0', 'origin-top-left', 'origin-top-right');
+        
+        // Set default based on align prop
+        if (this.align === 'left') {
+            menu.classList.add('left-0', 'origin-top-left');
+        } else {
+            menu.classList.add('right-0', 'origin-top-right');
+        }
+        
+        let rect = menu.getBoundingClientRect();
+        
+        if (this.align === 'left') {
+            if (rect.right > window.innerWidth) {
+                menu.classList.remove('left-0', 'origin-top-left');
+                menu.classList.add('right-0', 'origin-top-right');
+            }
+        } else {
+            if (rect.left < 0) {
+                menu.classList.remove('right-0', 'origin-top-right');
+                menu.classList.add('left-0', 'origin-top-left');
+            }
+        }
+    },
     getVisibleItems() {
         return Array.from(this.$refs.menuContainer ? this.$refs.menuContainer.querySelectorAll('[role=\'menuitem\'], button, a') : [])
             .filter(el => el.offsetWidth > 0 || el.offsetHeight > 0);
@@ -58,6 +96,7 @@
     @keydown.down.window="focusNext($event)"
 @endif 
 @click.outside="close()" 
+@resize.window="open ? adjustPosition() : null"
 {{ $attributes }}>
     
     @if ($label || isset($trigger))
