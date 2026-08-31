a0df8d38f83f6ae20238bb2fad02981560bcaa5e
Fahril Refiandi <fahril.refandi@gmail.com>
2026-09-01 00:26:45 +0700

    chore: update default font to Figtree and expand included weights

--- vite.config.js
+++ vite.config.js
@@ -108,8 +108,8 @@ export default defineConfig({
             ],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
+                bunny('Figtree', {
+                    weights: [400, 500, 600, 700],
                 }),
             ],
         }),
