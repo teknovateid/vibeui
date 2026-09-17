82732a578c28fb735dc5803e24d516ceb15b3fc5
Fahril Refiandi <fahril.refandi@gmail.com>
2026-09-17 07:09:19 +0700

    chore(release): v0.2.1

--- src/Vibe.php
+++ src/Vibe.php
@@ -9,7 +9,7 @@ class Vibe
     /**
      * The current version of Vibe UI.
      */
-    const VERSION = '0.2.0';
+    const VERSION = '0.2.1';
 
     /**
      * The custom model class for Two Factor Authenticator.
